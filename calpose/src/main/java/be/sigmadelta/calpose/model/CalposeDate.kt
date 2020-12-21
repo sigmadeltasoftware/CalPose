f33f9fe77b0ac4995be915143cbefc2ae9f48b64
Bojan Belic <belic_bojan@sigmadelta.be>
2020-12-21 10:36:09 +0100

    Added dokka for CalposeDate

--- calpose/src/main/java/be/sigmadelta/calpose/model/CalposeDate.kt
+++ calpose/src/main/java/be/sigmadelta/calpose/model/CalposeDate.kt
@@ -3,4 +3,14 @@ package be.sigmadelta.calpose.model
 import java.time.DayOfWeek
 import java.time.YearMonth
 
+/**
+ * Contains metadata information about a certain day
+ *
+ * @property day Indicates the day number (1-31)
+ *
+ * @property dayOfWeek Contains DayOfWeek information about the day (Monday-Sunday)
+ *
+ * @property day Information about the month the date belongs to
+ *
+ */
 data class CalposeDate (val day: Int, val dayOfWeek: DayOfWeek, val month: YearMonth)
