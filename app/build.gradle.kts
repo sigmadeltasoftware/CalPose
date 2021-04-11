fb4f701bf394ae5dbc9e483e3163b17f73f995b7
Wise86 <wise86android@gmail.com>
2021-04-11 14:21:16 +0200

    remove the unused isCoreLibraryDesugaringEnabled setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,6 @@ android {
     }
 
     compileOptions {
-        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
